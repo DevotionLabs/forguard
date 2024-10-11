cbe6fff783f43ca40038846d2b3b3f6bdc781f8a
diegoundevfined <157759628+diegoundevfined@users.noreply.github.com>
2024-10-11 14:25:16 +0200

    Add parent `ComposeEditor` (#8)
    * Fix excluded coverage paths
    
    * Add compose path shortcut
    
    * Add js-yaml library
    
    * Add docker compose spec types generation
    
    * Add compose editor class
    
    * Add compose editor tests

---
+++ forguarder/packages/api/src/services/docker/compose/editor/ComposeEditor.ts
@@ -0,0 +1,43 @@
+import { readFileSync, writeFileSync } from "fs";
+import yaml from "js-yaml";
+import { Logger } from "@logger/index";
+import { ComposeSpecification } from "./generated/types";
+
+export class ComposeEditor {
+  private path: string;
+  private compose: ComposeSpecification;
+
+  constructor(path: string) {
+    this.path = path;
+    this.compose = this.loadFromFile();
+  }
+
+  private loadFromFile(): ComposeSpecification {
+    try {
+      return this.parseComposeFile();
+    } catch (error) {
+      Logger.error(`Error loading the compose file: ${error}`);
+      throw error;
+    }
+  }
+
+  private parseComposeFile(): ComposeSpecification {
+    const fileContents = readFileSync(this.path, "utf8");
+    return yaml.load(fileContents) as ComposeSpecification;
+  }
+
+  protected saveToFile() {
+    try {
+      this.writeComposeToFile();
+    } catch (error) {
+      Logger.error(`Error saving the compose file: ${error}`);
+      throw error;
+    }
+  }
+
+  private writeComposeToFile(): void {
+    const yamlStr = yaml.dump(this.compose);
+    writeFileSync(this.path, yamlStr, "utf8");
+    Logger.info(`Successfully written compose to ${this.path}`);
+  }
+}
