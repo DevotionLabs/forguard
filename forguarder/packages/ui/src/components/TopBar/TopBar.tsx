4b8b08ba76e17fad1dbe0e31958929bdbc144983
diego <diego.undevfined@gmail.com>
2025-04-04 01:08:44 +0200

    Define UI simple template

---
+++ forguarder/packages/ui/src/components/TopBar/TopBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TopBar() {
+	return (
+		<header className="w-full bg-gray-800 text-white py-4 px-8">
+			<div className="container mx-auto flex justify-between items-center">
+				<h1 className="text-xl font-bold">ForGuard</h1>
+			</div>
+		</header>
+	);
+}
